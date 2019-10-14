e7a73e7c36c0b62e2ce9968798e6c6be5c91fc34
kuramafc <brunogiamm@hotmail.com>
2019-10-13 22:51:59 -0300

    Initial commit

---
+++ RedisApplicationTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RedisApplicationTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IDistributedCache _cache;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDistributedCache cache)
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var result0 = _cache.GetAsync("ABACAXI").GetAwaiter().GetResult();
+
+            _cache.SetStringAsync("ABACAXI", "abestado",new DistributedCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(1))).GetAwaiter().GetResult();
+
+            var result = _cache.GetStringAsync("ABACAXI").GetAwaiter().GetResult();
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
